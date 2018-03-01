41e5d2a662be480e42170b78c1eafc400a1d4473
Paulo Montarroios <pauloricardomontarroios@hotmail.com>
2018-03-01 16:24:06 -0300

    Criando uma representação para o recurso Livro

--- src/main/java/com/algaworks/socialbook/resources/LivrosResource.java
+++ src/main/java/com/algaworks/socialbook/resources/LivrosResource.java
@@ -1,14 +1,26 @@
 package com.algaworks.socialbook.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.socialbook.domain.Livro;
 
 @RestController
 public class LivrosResource {
 
 	@RequestMapping(value="/livros", method = RequestMethod.GET)
-	public String listar() {
-		return "Rest aplicado, Git-Git Passo-a-Passo";
+	public List<Livro> listar() {
+
+		Livro l1 = new Livro("RESTful aplicado");
+		Livro l2 = new Livro("Git passo-a-passo");
+		
+		Livro[] livros = {l1,l2};
+		
+		return Arrays.asList(livros);
 	}
 }
